bc06064b871db63fdeb29e0b5f20909fdb30156a
Rakibul-Hossain <rakibul.hossain@appscode.com>
2022-07-24 02:58:52 +0600

    Fix: Fenwick tree bug
    Signed-off-by: Rakibul-Hossain <rakibul.hossain@appscode.com>

--- Data-Structure/fenwick.cpp
+++ Data-Structure/fenwick.cpp
@@ -6,36 +6,34 @@ const int siz=2e5+7;
 // 0 based indexing query idx=(idx & (idx+1))-1 amd update idx = (idx | (idx+1) )
 // 1 based indexing query idx-=(idx & -idx) and update idx+=(idx & -idx)
 // complexity O(logn) per query and update and space complexity O(n)
-int n;
 type bit1[siz],bit2[siz];
-type get(type *bit,int x)
+type get(type *bit,int idx)
 {
     type ret = 0;
-    for(int i=x; i>=0; i=(i&(i+1))-1) ret+=bit[i];
+    for(int i=idx; i>=0; i=(i&(i+1))-1) ret+=bit[i];
     return ret;
 }
-void updt(type *bit,int x,int val)
+void updt(type *bit,int idx,int val)
 {
-    for(int i=x;i<n;i|=i+1) bit+=val;
+    for(int i=idx;i<siz;i|=i+1) bit[i]+=val;
 }
 
-void range_updt(int l,int r,int x)
+void range_updt(int l,int r,int val)
 {
-    updt(bit1,l,x);
-    updt(bit1,r+1,-x);
-    updt(bit2,l,x*(l-1));
-    updt(bit2,r+1,-x*r);
+    updt(bit1,l,val);
+    updt(bit1,r+1,-val);
+    updt(bit2,l,val*(l-1));
+    updt(bit2,r+1,-val*r);
 }
-type gt(int x)
+type gt(int idx)
 {
-    return get(bit1,x)*x-get(bit2,x);
+    return get(bit1,idx)*idx-get(bit2,idx);
 }
-type query(int x,int y)
+type query(int l,int r)
 {
-    return gt(y)-gt(x-1);
+    return gt(r)-gt(l-1);
 }
 int main()
 {
-
     return 0;
 }
